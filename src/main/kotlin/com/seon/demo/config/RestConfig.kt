d7cc1345c5fa54a202aa12bd2dd72eb2019d246c
seon <sebastian@seonsoftwarefactory.com>
2019-12-25 17:07:31 +0000

    configure timeouts

--- src/main/kotlin/com/seon/demo/config/RestConfig.kt
+++ src/main/kotlin/com/seon/demo/config/RestConfig.kt
@@ -1,12 +1,28 @@
 package com.seon.demo.config
 
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestTemplate
+import java.time.Duration.ofSeconds
+
 
 @Configuration
 class RestConfig {
 
+    @Value("\${custom.rest.connection.connect-timeout}")
+    lateinit var connectTimeout: String
+
+    @Value("\${custom.rest.connection.read-timeout}")
+    lateinit var readTimeout: String
+
     @Bean
-    fun restTemplate() = RestTemplate()
+    fun restTemplate(restTemplateBuilder: RestTemplateBuilder): RestTemplate {
+        return restTemplateBuilder
+                .setConnectTimeout(ofSeconds(connectTimeout.toLong()))
+                .setReadTimeout(ofSeconds(readTimeout.toLong()))
+                .build()
+    }
+
 }
--- src/test/kotlin/com/seon/demo/movie/MovieClientComponentTest.kt
+++ src/test/kotlin/com/seon/demo/movie/MovieClientComponentTest.kt
@@ -1,5 +1,6 @@
 package com.seon.demo.movie
 
+import arrow.core.Left
 import arrow.core.Right
 import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.client.WireMock.aResponse
@@ -30,10 +31,23 @@ class MovieClientComponentTest {
         AssertionsForClassTypes.assertThat(movieResult).isEqualTo(Right(movie))
     }
 
-    private fun stubCallToMovieCatalogue() {
+    @Test
+    fun `should timeout after 3 seconds`() {
+        stubCallToMovieCatalogue(3000)
+
+        val errorMessage = "I/O error on GET request for \"http://localhost:9999/movie-catalogue/StarWars\": Read timed out; nested exception is java.net.SocketTimeoutException: Read timed out"
+        val movie = ConnectionRefused(message = errorMessage)
+        val movieResult = movieClient.callMovieService("StarWars")
+
+        assertThat(movieResult.isLeft()).isTrue()
+        AssertionsForClassTypes.assertThat(movieResult).isEqualTo(Left(movie))
+    }
+
+    private fun stubCallToMovieCatalogue(delay: Int = 0) {
         WireMock.stubFor(WireMock.get(urlEqualTo("/movie-catalogue/StarWars"))
                 .willReturn(aResponse()
                         .withHeader("Content-Type", "application/json")
+                        .withFixedDelay(delay)
                         .withBody(" { \n" +
                                 "  \"title\":\"Star Wars\",\n" +
                                 "  \"director\":\"J. J. Abrams\"\n" +
